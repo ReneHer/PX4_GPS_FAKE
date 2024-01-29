1f87a34c2e21a3f42e0e5f971f85953f607b4c82
ReneHer <rene.hertwig@campus.tu-berlin.de>
2024-01-29 23:18:22 +0100

    Arduino slace communication with PX4 over I2C improved

--- src/drivers/gps_fake/dwm1004c/DWM1004C.cpp
+++ src/drivers/gps_fake/dwm1004c/DWM1004C.cpp
@@ -95,20 +95,21 @@ void DWM1004C::RunImpl()
 				// Send the command to begin a measurement (Read_MR)
 				uint8_t cmd = ADDR_READ_MR;
 
+				if (dwm_errors == MAX_ERRORS)
+				{
+					cmd = DW_RESET;
+					transfer(&cmd, 1, nullptr, 0);
+					dwm_errors = 0;
+					ScheduleDelayed(10_ms); // try again in 10 ms
+					cmd = ADDR_READ_MR;
+				}
+
 				if (transfer(&cmd, 1, nullptr, 0) == OK)
 				{
 					_timestamp_sample = hrt_absolute_time();
 					_state = STATE::READ;
 					ScheduleDelayed(2_ms);
 
-				}
-				else if (dwm_errors == MAX_ERRORS)
-				{
-					cmd = DW_RESET;
-					transfer(&cmd, 1, nullptr, 0);
-					dwm_errors = 0;
-					_state = STATE::MEASURE;
-					ScheduleDelayed(10_ms); // try again in 10 ms
 				}
 				else
 				{
@@ -150,7 +151,7 @@ void DWM1004C::RunImpl()
 					const uint8_t anchors_used_data_2 = data_2[1];
 
 					Vector<uint8_t, LOCODECK_NR_OF_TWR_ANCHORS> anchors_used;
-					for (uint8_t i = LOCODECK_NR_OF_TWR_ANCHORS-1; i == 0; i--) // Vergleich mit Code bei DWM1004C: Byte wird von vorn order hinten befüllt??
+					for (int8_t i = LOCODECK_NR_OF_TWR_ANCHORS-1; i >= 0; i--) // Vergleich mit Code bei DWM1004C: Byte wird von vorn order hinten befüllt??
 					{
 						anchors_used(i) = (anchors_used_data_1 >> i) & 0x01;
 					}
@@ -167,8 +168,8 @@ void DWM1004C::RunImpl()
 					}
 
 					// Checksum
-					const uint16_t checksum_1 = (data_1[DATA_RX_LENGTH-2] << 8) + data_1[DATA_RX_LENGTH-1];
-					const uint16_t checksum_2 = (data_2[DATA_RX_LENGTH-2] << 8) + data_2[DATA_RX_LENGTH-1];
+					const uint16_t checksum_1 = (data_1[DATA_RX_LENGTH-1] << 8) + data_1[DATA_RX_LENGTH-2];
+					const uint16_t checksum_2 = (data_2[DATA_RX_LENGTH-1] << 8) + data_2[DATA_RX_LENGTH-2];
 					// const uint8_t checksum_1_msb = data_1[DATA_RX_LENGTH-2];
 					// const uint8_t checksum_2_msb = data_2[DATA_RX_LENGTH-2];
 
@@ -179,7 +180,7 @@ void DWM1004C::RunImpl()
 
 					uint16_t checksum_RX_1 = 0;
 					uint16_t checksum_RX_2 = 0;
-					for (uint8_t i = 0; i < DATA_RX_LENGTH; i++)
+					for (uint8_t i = 1; i < DATA_RX_LENGTH-2; i++)
 					{
 						checksum_RX_1 += data_1[i];
 						checksum_RX_2 += data_2[i];
@@ -188,17 +189,28 @@ void DWM1004C::RunImpl()
 					// uint32_t measurement_end_1 = measurement_1(LOCODECK_NR_OF_TWR_ANCHORS);
 					// uint32_t measurement_end_2 = measurement_2(LOCODECK_NR_OF_TWR_ANCHORS);
 
+					PX4_INFO_RAW("status:%X|%X, anchors_used:%X|%X, checksum:%X|%X, checksum_RX:%X|%X\n", status_1, status_2, anchors_used_data_1, anchors_used_data_2, checksum_1, checksum_2, checksum_RX_1, checksum_RX_2); // LOCODECK_NR_OF_TWR_ANCHORS,
+					for (uint8_t i = 0; i < LOCODECK_NR_OF_TWR_ANCHORS; i++)
+					{
+						PX4_INFO_RAW("anchors_used[%d] = %d\n", i, anchors_used(i));
+					}
+					for (uint8_t i = 0; i < LOCODECK_NR_OF_TWR_ANCHORS; i++)
+					{
+						PX4_INFO_RAW("measurement_1[%d] = %f\n", i, (double)measurement_1(i));
+					}
+
 					if ((status_1 == (uint8_t)STATUS::Fault_Detected) || (status_2 == (uint8_t)STATUS::Fault_Detected))
 					{
 						// Fault Detected
 						perf_count(_fault_perf);
 						dwm_errors++;
+						PX4_INFO_RAW("Fault Detected\n");
 					}
 					else if ((status_1 == (uint8_t)STATUS::Normal_Operation) && (status_2 == (uint8_t)STATUS::Stale_Data)
-						&& (anchors_used_data_1 == anchors_used_data_2)&& (((checksum_1 == checksum_2) == checksum_RX_1) == checksum_RX_2)) //  && (measurement_end_1 == measurement_end_2)
+						&& (anchors_used_data_1 == anchors_used_data_2) && (checksum_1 == checksum_RX_2)) // && (measurement_end_1 == measurement_end_2)
 					{
 						check_data = false;
-
+						// PX4_INFO_RAW("checksum: %X|%X, checksum_RX: %X|%X\n", checksum_1, checksum_2, checksum_RX_1, checksum_RX_2); // LOCODECK_NR_OF_TWR_ANCHORS,
 						Matrix<float, LOCODECK_NR_OF_TWR_ANCHORS, 3> anchorPosition(anchorPosition_data);
 
 						anchors_used_sum = anchors_used.dot(anchors_used);
@@ -207,38 +219,41 @@ void DWM1004C::RunImpl()
 						{
 							measurements_good++;
 
-							double delta_i = 10.0;
-							uint8_t i = 0;
-							Vector3f x_ccf_i_plus_1(x_N);
+							PX4_INFO_RAW("anchors_used_sum = %d\n", anchors_used_sum);
+							// double delta_i = 10.0;
+							// uint8_t i = 0;
+							// Vector3f x_ccf_i_plus_1(x_N);
 
-							while (delta_i > 0.001 && i < 100)
-							{
-								Vector3f x_ccf_i(x_ccf_i_plus_1);
-								x_ccf_i_plus_1 = dme_least_squares(x_ccf_i, anchorPosition.transpose(), measurement_1, anchors_used);
-								delta_i = Vector3f (x_ccf_i_plus_1 - x_ccf_i).norm();
-								i++;
-							}
+							// while (delta_i > 0.001 && i < 100)
+							// {
+							// 	Vector3f x_ccf_i(x_ccf_i_plus_1);
+							// 	x_ccf_i_plus_1 = dme_least_squares(x_ccf_i, anchorPosition.transpose(), measurement_1, anchors_used);
+							// 	delta_i = Vector3f (x_ccf_i_plus_1 - x_ccf_i).norm();
+							// 	i++;
+							// }
 
-							if (i == 100)
-							{
-								// PX4_ERR("Error: No convergence of DME-Least-Squares-Algorithm\n");
-								check_data = false;
-								perf_count(_fault_perf);
-								dwm_errors++;
-							}
-							else
-							{
-								check_data = true;
-								vel_N = (x_ccf_i_plus_1 - x_N) / ((double)SENSOR_INTERVAL_US * 1e-6);
-								x_N = x_ccf_i_plus_1;
-								dwm_errors = 0;
-							}
+							// if (i == 100)
+							// {
+							// 	// PX4_ERR("Error: No convergence of DME-Least-Squares-Algorithm\n");
+							// 	check_data = false;
+							// 	perf_count(_fault_perf);
+							// 	dwm_errors++;
+							// }
+							// else
+							// {
+							// 	check_data = true;
+							// 	vel_N = (x_ccf_i_plus_1 - x_N) / ((double)SENSOR_INTERVAL_US * 1e-6);
+							// 	x_N = x_ccf_i_plus_1;
+							// 	dwm_errors = 0;
+							// }
 						}
 						else
 						{
 							check_data = false;
 							perf_count(_fault_perf);
 							dwm_errors++;
+							PX4_INFO_RAW("Error: Not enough anchors used\n");
+							PX4_INFO_RAW("dwm_errors = %d\n", dwm_errors);
 						}
 					}
 					else
@@ -247,108 +262,115 @@ void DWM1004C::RunImpl()
 							anchors_used_data_1, anchors_used_data_2, checksum_1, checksum_2, checksum_RX_1, checksum_RX_2); // LOCODECK_NR_OF_TWR_ANCHORS,
 							//(double)measurement_1(LOCODECK_NR_OF_TWR_ANCHORS), (double)measurement_2(LOCODECK_NR_OF_TWR_ANCHORS));
 						dwm_errors++;
+						PX4_INFO_RAW("Error: Status, Anchors, Checksum or Measurement not OK\n");
 					}
 				}
 
 				_state = STATE::MEASURE;
 				ScheduleDelayed(10_ms);
 
+				ScheduleDelayed(5000_ms);
+
 				break;
 			}
 	}
 
-	// Reset von DWM1004C ermöglichen
-	// Was passiert, wenn der GPS-Signal kacke ist oder keins vorhanden ist?
-	// Standardabweichung der Messung, Rauschen, Messunsicherheit
-	// Filterung
-	// positionsausreisser filtern, wenn die position zu weit von der letzten position entfernt ist, mit gleitendem mittelwert oder tiefpass filtern
-	// geschwindigkeitsausreisser filtern, wenn die geschwindigkeit zu weit von der letzten geschwindigkeit entfernt ist, mit gleitendem mittelwert oder tiefpass filtern
+	// // Reset von DWM1004C ermöglichen
+	// // Was passiert, wenn der GPS-Signal kacke ist oder keins vorhanden ist?
+	// // Standardabweichung der Messung, Rauschen, Messunsicherheit
+	// // Filterung
+	// // positionsausreisser filtern, wenn die position zu weit von der letzten position entfernt ist, mit gleitendem mittelwert oder tiefpass filtern
+	// // geschwindigkeitsausreisser filtern, wenn die geschwindigkeit zu weit von der letzten geschwindigkeit entfernt ist, mit gleitendem mittelwert oder tiefpass filtern
 
-	double latitude = 0.0;
-	double longitude = 0.0;
-	double altitude = 0.0;
+	// double latitude = 0.0;
+	// double longitude = 0.0;
+	// double altitude = 0.0;
 
-	double x_fe = (double)x_N(0);
-	double y_fe = (double)-x_N(1);
-	double z_fe = (double)-x_N(2);
+	// double x_fe = (double)x_N(0);
+	// double y_fe = (double)-x_N(1);
+	// double z_fe = (double)-x_N(2);
 
-	double Psi = 0.0;
-	flat_earth_to_lla(lla_0(0), lla_0(1), lla_0(2), x_fe, y_fe, z_fe, Psi, latitude, longitude, altitude);
+	// double Psi = 0.0;
+	// flat_earth_to_lla(lla_0(0), lla_0(1), lla_0(2), x_fe, y_fe, z_fe, Psi, latitude, longitude, altitude);
 
-	Vector3f gps_vel = Vector3f(0.0, 0.0, 0.0);
-	gps_vel(0) =  vel_N(0);
-	gps_vel(1) = -vel_N(1);
-	gps_vel(2) = -vel_N(2);
+	// Vector3f gps_vel = Vector3f(0.0, 0.0, 0.0);
+	// gps_vel(0) =  vel_N(0);
+	// gps_vel(1) = -vel_N(1);
+	// gps_vel(2) = -vel_N(2);
 
-	// device id
-	device::Device::DeviceId device_id;
-	device_id.devid_s.bus_type = device::Device::DeviceBusType::DeviceBusType_I2C; // DeviceBusType_UNKNOWN
-	device_id.devid_s.bus = 0;
-	device_id.devid_s.address = 0;
-	device_id.devid_s.devtype = DRV_GPS_DEVTYPE_DWM1004C;
+	// // device id
+	// device::Device::DeviceId device_id;
+	// device_id.devid_s.bus_type = device::Device::DeviceBusType::DeviceBusType_I2C; // DeviceBusType_UNKNOWN
+	// device_id.devid_s.bus = 0;
+	// device_id.devid_s.address = 0;
+	// device_id.devid_s.devtype = DRV_GPS_DEVTYPE_DWM1004C;
 
-	sensor_gps_s sensor_gps{};
+	// sensor_gps_s sensor_gps{};
 
-	if (anchors_used_sum >= 4)
-	{
-		// fix
-		sensor_gps.s_variance_m_s = 0.4f;
-		sensor_gps.c_variance_rad = 0.1f;
-		sensor_gps.eph = 0.9f;				//0.3f
-		sensor_gps.epv = 1.78f;				//0.3f
-		sensor_gps.hdop = 0.7f;
-		sensor_gps.vdop = 1.1f;
+	// if (anchors_used_sum >= 4)
+	// {
+	// 	// fix
+	// 	sensor_gps.s_variance_m_s = 0.4f;
+	// 	sensor_gps.c_variance_rad = 0.1f;
+	// 	sensor_gps.eph = 0.9f;				//0.3f
+	// 	sensor_gps.epv = 1.78f;				//0.3f
+	// 	sensor_gps.hdop = 0.7f;
+	// 	sensor_gps.vdop = 1.1f;
 
-		sensor_gps.fix_type = 3; // 3D fix
-	}
-	else
-	{
-		// no fix
-		sensor_gps.s_variance_m_s = 100.f;
-		sensor_gps.c_variance_rad = 100.f;
-		sensor_gps.eph = 100.f;
-		sensor_gps.epv = 100.f;
-		sensor_gps.hdop = 100.f;
-		sensor_gps.vdop = 100.f;
+	// 	sensor_gps.fix_type = 3; // 3D fix
+	// }
+	// else
+	// {
+	// 	// no fix
+	// 	sensor_gps.s_variance_m_s = 100.f;
+	// 	sensor_gps.c_variance_rad = 100.f;
+	// 	sensor_gps.eph = 100.f;
+	// 	sensor_gps.epv = 100.f;
+	// 	sensor_gps.hdop = 100.f;
+	// 	sensor_gps.vdop = 100.f;
 
-		sensor_gps.fix_type = 0; // No fix
-	}
+	// 	sensor_gps.fix_type = 0; // No fix
+	// }
 
+	// //
+	// sensor_gps.timestamp_sample = _timestamp_sample; // = 0 // gpos.timestamp_sample;
+	// sensor_gps.latitude_deg = latitude; // Latitude in degrees
+	// sensor_gps.longitude_deg = longitude; // Longitude in degrees
+	// sensor_gps.altitude_msl_m = altitude; // Altitude in meters above MSL
+	// sensor_gps.altitude_ellipsoid_m = altitude;
+	// sensor_gps.time_utc_usec = hrt_absolute_time(); // hrt_absolute_time() + 1613692609599954; // 0;
+	// sensor_gps.device_id = device_id.devid;
+	// /*
+	// ...
+	// */
+	// sensor_gps.noise_per_ms = 0;
+	// sensor_gps.jamming_indicator = 0;
+	// sensor_gps.vel_m_s = sqrtf(gps_vel(0) * gps_vel(0) + gps_vel(1) * gps_vel(1)); // GPS ground speed, (metres/sec)
+	// sensor_gps.vel_n_m_s = gps_vel(0);
+	// sensor_gps.vel_e_m_s = gps_vel(1);
+	// sensor_gps.vel_d_m_s = gps_vel(2);
+	// sensor_gps.cog_rad = atan2(gps_vel(1), gps_vel(0)); // Course over ground (NOT heading, but direction of movement), -PI..PI, (radians)
+	// sensor_gps.timestamp_time_relative = 0;
+	// sensor_gps.heading = NAN;
+	// sensor_gps.heading_offset = NAN;
+	// sensor_gps.heading_accuracy = 0;
+	// //
+	// sensor_gps.automatic_gain_control = 0;
+	// //
+	// sensor_gps.jamming_state = 0;
+	// sensor_gps.spoofing_state = 0;
+	// sensor_gps.vel_ned_valid = true;
+	// sensor_gps.satellites_used = anchors_used_sum;
+	// /*
+	// ...
+	// */
+	// sensor_gps.timestamp = hrt_absolute_time();
+
+	// if (hrt_elapsed_time(&_timestamp_sample) < 20_ms)
+	// {
 	//
-	sensor_gps.timestamp_sample = _timestamp_sample; // = 0 // gpos.timestamp_sample;
-	sensor_gps.latitude_deg = latitude; // Latitude in degrees
-	sensor_gps.longitude_deg = longitude; // Longitude in degrees
-	sensor_gps.altitude_msl_m = altitude; // Altitude in meters above MSL
-	sensor_gps.altitude_ellipsoid_m = altitude;
-	sensor_gps.time_utc_usec = hrt_absolute_time(); // hrt_absolute_time() + 1613692609599954; // 0;
-	sensor_gps.device_id = device_id.devid;
-	/*
-	...
-	*/
-	sensor_gps.noise_per_ms = 0;
-	sensor_gps.jamming_indicator = 0;
-	sensor_gps.vel_m_s = sqrtf(gps_vel(0) * gps_vel(0) + gps_vel(1) * gps_vel(1)); // GPS ground speed, (metres/sec)
-	sensor_gps.vel_n_m_s = gps_vel(0);
-	sensor_gps.vel_e_m_s = gps_vel(1);
-	sensor_gps.vel_d_m_s = gps_vel(2);
-	sensor_gps.cog_rad = atan2(gps_vel(1), gps_vel(0)); // Course over ground (NOT heading, but direction of movement), -PI..PI, (radians)
-	sensor_gps.timestamp_time_relative = 0;
-	sensor_gps.heading = NAN;
-	sensor_gps.heading_offset = NAN;
-	sensor_gps.heading_accuracy = 0;
-	//
-	sensor_gps.automatic_gain_control = 0;
-	//
-	sensor_gps.jamming_state = 0;
-	sensor_gps.spoofing_state = 0;
-	sensor_gps.vel_ned_valid = true;
-	sensor_gps.satellites_used = anchors_used_sum;
-	/*
-	...
-	*/
-	sensor_gps.timestamp = hrt_absolute_time();
-
-	_sensor_gps_pub.publish(sensor_gps);
+	// }
+	// _sensor_gps_pub.publish(sensor_gps);
 
 	perf_end(_loop_perf);
 }
